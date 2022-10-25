7c8342d6bb7c3f3426a01e5bb90a85d1e6140e8a
Jan0Fu <115988224+Jan0Fu@users.noreply.github.com>
2022-10-25 15:07:53 +0200

    Finished main parts of program, but code still not executable

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -3,33 +3,51 @@ function computerChoice() {
     let random = myArray[Math.floor(Math.random() * myArray.length)];
     return random;
 }
+let computerPoints = 0;
+let playerPoints = 0;
+let computerSelection = computerChoice();
+//let playerChoice = prompt("rock?, paper? or scissors?");
+//let playerSelection = playerChoice.toLowerCase();
+let playerSelection = "rock"
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         return "It's a tie";
     } else if (playerSelection === "rock" && computerSelection === "paper") {
+        ++computerPoints;
         return "You lose, paper beats rock";
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        ++playerPoints;
         return "You win, rock beats scissors"
     } else if (playerSelection === "paper" && computerSelection === "rock") {
+        ++playerPoints;
         return "You win, paper beats rock";
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        ++computerPoints;
         return "You lose, scissors beats paper";
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        ++computerPoints;
         return "You lose, rock beats scissors";
     } else if (playerSelection === "scissors" && computerSelection === "scissors") {
+        ++playerPoints;
         return "You win, rock beats scissors" ;
     }
   
 }
-let computerSelection = computerChoice();
-let playerChoice = prompt("rock?, paper? or scissors?");
-let playerSelection = playerSelection.toLowerCase();
-console.log(playRound(playerSelection, computerSelection));
-
-/*let counter
-  function game(playRound()) {
-    for (let i = 0; i < 5; i++)
+//console.log(playRound(playerSelection, computerSelection));
+let result = playRound(playerSelection, computerSelection);
+let counter = 0;
+function Game() {
+  for (let i = 0; i < 5; i++) {
+    if (result.includes("win")) {
+        counter += 1;
+    } else if (result.includes("lose")) {
+        counter -= 1;
+    } else {
+        counter += 0;
+    }
   }
+  return counter;
+}
 
-*/ 
+
