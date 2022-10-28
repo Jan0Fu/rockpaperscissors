b713ece9ead0144bc67cdce0d347747569a8c01e
Jan0Fu <115988224+Jan0Fu@users.noreply.github.com>
2022-10-28 13:28:01 +0200

    Added simple gui to rock paper scissors game

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,47 +1,54 @@
-let computerPoints = 0;
-let playerPoints = 0;
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+const currentRound = document.querySelector('#currentRound');
+const score = document.querySelector('#score');
+
+rock.addEventListener('click', function () {
+    playRound('rock', computerChoice())
+});
+paper.addEventListener('click', function () {
+    playRound('paper', computerChoice())
+});
+scissors.addEventListener('click', function () {
+    playRound('scissors', computerChoice())
+});
+
+let computerScore = 0;
+let playerScore = 0;
 function computerChoice() {
     let myArray = ["rock", "paper", "scissors"];
     let randomNum = Math.floor(Math.random() * myArray.length);
     return myArray[randomNum];
 }
+function gameOver() {
+    return playerScore === 5 || computerScore === 5;
+}
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        return `It's a tie, you both picked ${playerSelection}`;
-    } else if (playerSelection === "rock" && computerSelection === "paper") {
-        computerPoints++;
-        return "You lose, paper beats rock";
-    } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        playerPoints++;
-        return "You win, rock beats scissors"
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-        playerPoints++;
-        return "You win, paper beats rock";
-    } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        computerPoints++;
-        return "You lose, scissors beats paper";
-    } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        computerPoints++;
-        return "You lose, rock beats scissors";
-    } else if (playerSelection === "scissors" && computerSelection === "scissors") {
-        playerPoints++;
-        return "You win, rock beats scissors" ;
+    if (!gameOver()) {
+        playerSelection = playerSelection.toLowerCase();
+        if (playerSelection == computerSelection) {
+            currentRound.textContent = `Draw! Both ${playerSelection}, play again`;
+        } else if ((playerSelection == "rock" && computerSelection == "scissors") || (playerSelection == "paper" &&
+                computerSelection == "rock") || (playerSelection == "scissors" && computerSelection == "paper")) {
+            currentRound.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
+            playerScore++;
+        } else {
+            currentRound.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
+            computerScore++;
+        }
+        score.textContent = updateScore()
+        if (playerScore === 5) {
+            currentRound.textContent = 'You won the game! Congratulations!'
+        }
+        else if (computerScore === 5) {
+            currentRound.textContent = 'You lost to a computer! Try again'
+        }
+    }
+    else {
+        alert('Game is over. Refresh page to play again')
     }
 }
-const game = () => {
-  for (let i = 0; i < 5; i++) {
-    const playerSelection = prompt("rock?, paper? or scissors?").toLowerCase();
-    const computerSelection = computerChoice();
-    playRound(playerSelection, computerSelection)
-  }
-    if (playerPoints > computerPoints) {
-        return "You beat computer, It's a Win!";
-    } else if (playerPoints < computerPoints) {
-        return "Computer beat you, you lost...";
-    } else {
-        return "It's a draw, not bad.";
-    }
-  }
-(game());
-
-
+function updateScore() {
+    return `Score is ${playerScore}-${computerScore}`
+}
